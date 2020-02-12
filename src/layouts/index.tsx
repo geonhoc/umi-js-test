cb4df838ea4abc1e5080beb7de6e73cd6cfdd17e
GeonHoC <geonhoc@tripbtoz.com>
2020-02-12 13:01:06 +0900

    antd로 디자인 변경

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,13 +3,13 @@ import styles from './index.css';
 import Link from 'umi/link';
 import '../../node_modules/antd/dist/antd.css';
 
-import { Menu, Icon } from 'antd';
-const { SubMenu } = Menu;
+import {Menu, Icon, Layout} from 'antd';
+const {SubMenu} = Menu;
+const {Header, Footer, Sider, Content} = Layout;
 
 interface State {
   collapsed: boolean;
 }
-
 
 interface Props {
   children: any;
@@ -17,67 +17,71 @@ interface Props {
 
 class BasicLayout extends React.Component<Props, State> {
 
-  constructor(props: Props) {
-    super(props);
-
-    this.setState({collapsed: false})
-  }
+  state: State = {
+    collapsed: false
+  };
 
   render() {
-    const { children } = this.props;
+    const {children} = this.props;
+    const {collapsed} = this.state;
 
     return (
-      <div className={styles.normal}>
-        <h1 className={styles.title}><span>Tripbtoz</span></h1>
-        <div className={styles.container}>
-          <div className={styles.leftMenu}>
-            <Menu
-              mode="inline"
-              theme="light"
-              inlineCollapsed={false}
-            >
-              <Menu.Item key="1">
-                <Link to='/option1'>
-                  <Icon type="pie-chart" />
-                  <span>Option 1</span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Link to='/option2'>
-                  <Icon type="desktop" />
-                  <span>Option 2</span>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Link to='/option3'>
-                  <Icon type="inbox" />
-                  <span>Option 3</span>
-                </Link>
-              </Menu.Item>
-              <SubMenu
-                key="sub1"
-                title={
-                  <span>
-                  <Icon type="mail" />
+      <Layout style={{minHeight: '100vh'}}>
+        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+          <div className={styles.logo} />
+          <Menu theme="dark" mode="inline">
+
+            <Menu.Item key="1">
+              <Link to='/option1'>
+                <Icon type="pie-chart" />
+                <span>Option 1</span>
+              </Link>
+            </Menu.Item>
+
+            <Menu.Item key="2">
+              <Link to='/option2'>
+                <Icon type="desktop"/>
+                <span>Option 2</span>
+              </Link>
+            </Menu.Item>
+
+            <Menu.Item key="3">
+              <Link to='/option3'>
+                <Icon type="file" />
+                <span>Option 3</span>
+              </Link>
+            </Menu.Item>
+
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="team" />
                   <span>Option 4</span>
                 </span>
-                }
-              >
-                <Menu.Item key="5"><Link to='/option4-1'>Option 4-1</Link></Menu.Item>
-                <Menu.Item key="6"><Link to='/option4-2'>Option 4-2</Link></Menu.Item>
-                <Menu.Item key="7"><Link to='/option4-3'>Option 4-3</Link></Menu.Item>
-                <Menu.Item key="8"><Link to='/option4-4'>Option 4-4</Link></Menu.Item>
-              </SubMenu>
-            </Menu>
-          </div>
-
-          <div className={styles.content}>
+              }
+            >
+              <Menu.Item key="4"><Link to='/option4-1'>Option 4-1</Link></Menu.Item>
+              <Menu.Item key="5"><Link to='/option4-2'>Option 4-2</Link></Menu.Item>
+              <Menu.Item key="6"><Link to='/option4-3'>Option 4-3</Link></Menu.Item>
+              <Menu.Item key="7"><Link to='/option4-4'>Option 4-4</Link></Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{background: '#fff', padding: 0}} />
+          <Content style={{margin: '0 16px'}}>
             {children}
-          </div>
-        </div>
-      </div>
+          </Content>
+          <Footer style={{textAlign: 'center'}}>GeonHo Workspace</Footer>
+        </Layout>
+      </Layout>
     );
   }
+
+  onCollapse = (collapsed: boolean): void => {
+    this.setState({collapsed});
+  };
 }
 
 export default BasicLayout;
