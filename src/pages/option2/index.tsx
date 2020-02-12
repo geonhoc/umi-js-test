a9b53bd25dc20006b8dcce3fbea1e1534036a02c
GeonHoC <geonhoc@tripbtoz.com>
2020-02-12 15:19:50 +0900

    api 를 활용한 검색 기능 추가

--- src/pages/option2/index.tsx
+++ src/pages/option2/index.tsx
@@ -1,12 +1,64 @@
 
 import React from 'react';
-import styles from './index.css';
+import Moment from 'moment';
+import {AutoComplete} from 'antd';
 
-export default function() {
-
-  return (
-    <div style={{ marginTop: 14, padding: 24, background: '#fff', minHeight: '100%' }}>
-      Option 2
-    </div>
-  );
+interface State {
+  value: string;
+  dataSource: Array<string>;
 }
+
+class Option2 extends React.Component<{}, State> {
+
+  state: State = {
+    value: '',
+    dataSource: [],
+  };
+
+  render() {
+    const {dataSource, value} = this.state;
+
+    return (
+      <div style={{marginTop: 14, padding: 24, background: '#fff', minHeight: '100%'}}>
+        <AutoComplete
+          dataSource={dataSource}
+          style={{width: 200}}
+          onSelect={this.onSelect}
+          onSearch={this.onSearch}
+          placeholder="검색"
+        />
+        <div><span>{value}</span></div>
+      </div>
+    );
+  }
+
+  onSearch = async (searchText: any): Promise<void> => {
+    if(String(searchText) !== '') {
+      const url = `/ajax/autocomplete?name=${searchText}`;
+      const data = await fetch(String(url)).then(res => res.json());
+
+      const list: Array<string> = [];
+
+      for(let key in data) {
+        if(data.hasOwnProperty(key)) {
+          if(key === 'data') {
+            const listData = data[key];
+            for(let i=0; i<listData.length; i++) {
+              list.push(listData[i].name);
+            }
+          }
+        }
+      }
+      
+      this.setState({
+        dataSource: list,
+      });
+    }
+  };
+
+  onSelect = (value: any): void => {
+    this.setState({value: value});
+  };
+}
+
+export default Option2;
