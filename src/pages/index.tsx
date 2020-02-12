267690a4f59c462a5ecaec812255279c892dcfc4
GeonHoC <geonhoc@tripbtoz.com>
2020-02-12 13:01:49 +0900

    Content 페이지 Text 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import styles from './index.css';
 export default function() {
 
   return (
-      <div className={styles.dataArea}>
-        메인 대시보드
-      </div>
+    <div style={{ marginTop: 14, padding: 24, background: '#fff', minHeight: '100%' }}>
+      Option 0
+    </div>
   );
 }
